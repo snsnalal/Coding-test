c8a6f292844a1aeac3f342725b2941e5962f71e3
snsnalal <62021242+snsnalal@users.noreply.github.com>
2022-03-02 19:22:34 +0900

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,7 @@
+public Boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i <= (int)Math.sqrt(num); i ++){
+            if(num % i == 0) cnt += 1; 
+        }
+        return cnt == 1;
+    }
